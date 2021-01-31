d277e8a35bfc003456439e9ac333e5fc5aaee30c
Juan Pablo Conejero <34621340+jpconher@users.noreply.github.com>
2021-01-31 20:27:41 +0100

    Update 1. Multiples of 3 and 5.py

--- 1. Multiples of 3 and 5.py
+++ 1. Multiples of 3 and 5.py
@@ -1,10 +1,7 @@
-import time
-
-start_time = time.time()
+# Problem 1
 
 sum_multiples = 0
 for i in range(1,1000):
     if (i % 3 == 0) or (i % 5 == 0):
         sum_multiples += i
 
-print("{:.5f}".format(time.time() - start_time))
