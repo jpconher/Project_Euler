a7064e2e12f6933eaef618607fb376b9c77c9fdf
Juan Pablo Conejero <34621340+jpconher@users.noreply.github.com>
2021-02-26 08:26:17 +0100

    Add files via upload

--- problems_11_to_20/14. Longest Collatz sequence.py
+++ problems_11_to_20/14. Longest Collatz sequence.py
@@ -1,20 +1,19 @@
 # Problem 14
 
-number_len_seq = {}
-for i in range(1,1000000):
+number_len = {1:1}
+for i in range(2,1000000):
     s = 1
     j = i
     while j>1:
-        if number_len_seq.get(j):
-            s = s + number_len_seq[j] - 1 
-            break 
-        if j%2==0:
+        if j%2==0:    
             j = j/2
+            s += 1
+            if number_len.get(j):
+                number_len[i] = s + number_len[j]
+                break 
         else:
             j = 3*j+1
-        s += 1
-    number_len_seq[i] = s
     
-print("The number with the longest chain:", max(number_len_seq, 
-                                                   key=number_len_seq.get))
+print("The number with the longest chain:", max(number_len, 
+                                                   key=number_len.get))
 
