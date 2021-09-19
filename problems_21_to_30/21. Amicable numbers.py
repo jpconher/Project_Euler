c058a5f403ddee18075a460bc6c55b0f4e5fc142
Juan Pablo Conejero <34621340+jpconher@users.noreply.github.com>
2021-09-19 13:37:31 +0100

    Add files via upload

--- problems_21_to_30/21. Amicable numbers.py
+++ problems_21_to_30/21. Amicable numbers.py
@@ -3,17 +3,23 @@
 # return factors of a number 
 def factors(number):
     factors = [1]
-    for i in range(2,round(number/2)+1):
-        if number % i == 0:
-            factors.append(i)
+    if number % 2 == 0: # even number
+        for i in range(2,round(number/2)+1):
+            if number % i == 0:
+                factors.append(i)
+    else: # odd number
+        for i in range(3,round(number/2)+1,2):
+            if number % i == 0:
+                factors.append(i)   
     return factors 
 
-amicable = [] # 1 is an amicable number
+amicable = []
 for i in range(1,10000):
     sum_factors = sum(factors(i))
     if i == sum(factors(sum_factors)) and i != sum_factors:
         amicable.append(i)
         
 print("The sum of all amicable numbers under 10000 is", sum(amicable))
+
         
     
